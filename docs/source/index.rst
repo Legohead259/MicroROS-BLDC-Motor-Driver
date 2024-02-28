3eefbb66e3d76e1e7267fc774434f4c74086f8a7
legohead259 <legohead259@gmail.com>
2024-02-28 13:56:06 -0500

    Added docs

---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. OlympianESC-Docs documentation master file, created by
+   sphinx-quickstart on Mon Feb 26 14:09:28 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to OlympianESC-Docs's documentation!
+============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   code/main_cpp
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
+/**
+ * @file main.cpp
+ * @brief The main execution for the OlympianESC.
+ * The `setup()` function initializes the Serial communication ports, state machine,
+ * Micro-ROS node, and SimpleFOC controller.
+ * 
+ * @author Braidan Duffy
+ * @copyright 2024
+ * 
+ * @version 1.0.0
+ */
+
 #include "micro_ros_bldc.h"
 #include "foc_bldc.h"
 #include "neopixel.h"
 
+/**
+ * This runs before `loop()` on MCU start
+*/
 void setup() {
     Wire.begin();
     Serial.begin(115200);
@@ -15,6 +30,9 @@ void setup() {
     focBLDCSetup();
 }
 
+/**
+ * This runs continually
+*/
 void loop() {
 
 }
